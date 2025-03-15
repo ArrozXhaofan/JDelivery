a51a8417d1f792b3d024046fa95e05db3f4d4b9b
Jeanpiere Laura <jean.ganador12@gmail.com>
2025-03-15 11:50:31 -0500

    Initial Commit

---
+++ JDelivery/JDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JDeliveryApp.swift
+//  JDelivery
+//
+//  Created by Jeanpiere Laura on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct JDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
